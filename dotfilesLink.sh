78b6a7a8ad38cac57367522e76cc74c900d77ddf
royus <vimvimmervimmest@gmail.com>
2016-09-24 00:10:06 +0900

    link

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,4 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.todo ~/.todo
-ln -sf ~/dotfiles/.inputrc ~/.inputrc
+# ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
