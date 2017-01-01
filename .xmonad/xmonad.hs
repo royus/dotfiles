337b1b5d4f678e5c14e54a33de2671e99100ab64
root <root@mypc.localdomain>
2017-01-01 14:32:05 +0900

    xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,20 @@
+import XMonad
+import XMonad.Layout
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.ManageDocks
+
+
+main = do
+	xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobarrc"
+	xmonad $ defaultConfig
+		{ terminal           = "xterm"
+		, modMask            = mod4Mask
+		, borderWidth        = 5
+		, normalBorderColor  = "#6633FF"
+		, focusedBorderColor = "#66FFFF"
+		, manageHook         = manageDocks <+> manageHook defaultConfig
+		, layoutHook         = spacing 10 $ gaps [(U,12),(D,0),(L,30),(R,30)] $ Tall 1 0.03 0.5
+		}
+-- avoidStruts
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,4 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.xmonad ~/.xmonad
