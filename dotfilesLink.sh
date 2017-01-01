8a11e8b95d7fd88bc8ff3d33608b3722a753e9c3
root <root@mypc.localdomain>
2017-01-01 14:40:36 +0900

    xbacklight

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.xmonad ~/.xmonad
+ln -sf ~/dotfiles/lighter.sh ~/lighter.sh
+ln -sf ~/dotfiles/darker.sh ~/darker.sh
