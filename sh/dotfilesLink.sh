35017e4dab4e189ba112001fb7b8fec5a15140a5
royus <vimvimmervimmest@gmail.com>
2017-03-27 11:02:54 +0900

    x

--- sh/dotfilesLink.sh
+++ sh/dotfilesLink.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 # dotfilesLink.sh
-# Last Change: 2017/03/27 (Mon) 10:58:21.
+# Last Change: 2017/03/27 (Mon) 11:02:02.
 
 mkdir ~/.config 2>/dev/null
 mkdir ~/.config/fish 2>/dev/null
+mkdir ~/.xmonad 2>/dev/null
+mkdir ~/.vifm 2>/dev/null
 
-ln -sf ~/dotfiles/.vimrc         ~/.vimrc
-ln -sf ~/dotfiles/.inputrc       ~/.inputrc
-ln -sf ~/dotfiles/.bashrc        ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile  ~/.bash_profile
-ln -sf ~/dotfiles/.xmonad        ~/.xmonad
-ln -sf ~/dotfiles/.stalonetrayrc ~/.stalonetrayrc
-ln -sf ~/dotfiles/sh/lighter.sh  ~/lighter.sh
-ln -sf ~/dotfiles/sh/darker.sh   ~/darker.sh
-ln -sf ~/dotfiles/.xinitrc       ~/.xinitrc
-ln -sf ~/dotfiles/.Xresources    ~/.Xresources
-ln -sf ~/dotfiles/.vifm          ~/.vifm
-ln -sf ~/dotfiles/config.fish    ~/.config/fish/config.fish
-ln -sf ~/dotfiles/.skk-jisyo     ~/.skk-jisyo
+ln -sf ~/dotfiles/.vimrc            ~/.vimrc
+ln -sf ~/dotfiles/.inputrc          ~/.inputrc
+ln -sf ~/dotfiles/.bashrc           ~/.bashrc
+ln -sf ~/dotfiles/.bash_profile     ~/.bash_profile
+ln -sf ~/dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -sf ~/dotfiles/.xmonad/xmobar.hs ~/.xmonad/xmobar.hs
+ln -sf ~/dotfiles/.stalonetrayrc    ~/.stalonetrayrc
+ln -sf ~/dotfiles/sh/lighter.sh     ~/lighter.sh
+ln -sf ~/dotfiles/sh/darker.sh      ~/darker.sh
+ln -sf ~/dotfiles/.xinitrc          ~/.xinitrc
+ln -sf ~/dotfiles/.Xresources       ~/.Xresources
+ln -sf ~/dotfiles/.vifm/vifmrc      ~/.vifm/vifmrc
+ln -sf ~/dotfiles/config.fish       ~/.config/fish/config.fish
+ln -sf ~/dotfiles/.skk-jisyo        ~/.skk-jisyo
