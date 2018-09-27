283eb7311e99db0b38a705ca579192ce4f155277
royus <>
2018-09-27 19:37:30 +0900

    x

--- sh/git/setup.sh
+++ sh/git/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # setup.sh
-# Last Change: 2018/09/27 (Thu) 19:07:07.
+# Last Change: 2018/09/27 (Thu) 19:37:02.
 
+git config --global user.name "royus"
 git config --global core.editor vim
