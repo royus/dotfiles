2ed1f459dfe7aecbec62fe222a29e1a802fa82a6
royus <>
2018-11-06 21:56:10 +0900

    x

--- template/template.c
+++ template/template.c
@@ -1,5 +1,5 @@
 /* filename.c */
-/* Last Change: 2018/11/06 (Tue) 20:47:15. */
+/* Last Change: 2018/11/06 (Tue) 21:11:42. */
 
 #include<stdio.h>
 /* #include<stdlib.h> */
@@ -10,6 +10,7 @@
 
 /* while(scanf("%d",&n)!=EOF){} */
 /* int n[10]={0}; */
+/* scanf(" %d",&n); */
 
 int main(void){
 	/* blabla */
