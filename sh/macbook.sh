e2c005b1ffca526f2507cecf1b0fc73010c28e08
royus <vimvimmervimmest@gmail.com>
2018-11-22 23:57:17 +0900

    x

--- sh/macbook.sh
+++ sh/macbook.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # macbook.sh
-# Last Change: 2018/11/13 (Tue) 20:45:30.
+# Last Change: 2018/11/13 (Tue) 21:18:16.
 
-ln -sf ~/dotfiles/.karabiner.json                  ~/.config/karabiner/karabiner.json
+ln -sf ~/dotfiles/.karabiner.json ~/.config/karabiner/karabiner.json
