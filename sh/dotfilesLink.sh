e79246afc8b08d9641f8991f3074f9b54e04e74c
royus <vimvimmervimmest@gmail.com>
2018-12-14 13:12:24 +0900

    x

--- sh/dotfilesLink.sh
+++ sh/dotfilesLink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # dotfilesLink.sh
-# Last Change: 2018/12/10 (Mon) 11:14:56.
+# Last Change: 2018/12/14 (Fri) 13:10:52.
 
 mkdir ~/.xmonad           2>/dev/null
 mkdir ~/.vifm             2>/dev/null
@@ -9,21 +9,23 @@ mkdir ~/.config/termite   2>/dev/null
 mkdir ~/.config/karabiner 2>/dev/null
 
 ln -sf ~/dotfiles/.vimrc                  ~/.vimrc
-ln -sf ~/dotfiles/.inputrc                ~/.inputrc
-ln -sf ~/dotfiles/.bashrc                 ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile           ~/.bash_profile
-ln -sf ~/dotfiles/.xmonad/xmonad.hs       ~/.xmonad/xmonad.hs
-ln -sf ~/dotfiles/.xmonad/xmobar.hs       ~/.xmonad/xmobar.hs
-ln -sf ~/dotfiles/.stalonetrayrc          ~/.stalonetrayrc
+ln -sf ~/dotfiles/linux/.inputrc          ~/.inputrc
+ln -sf ~/dotfiles/linux/.bashrc           ~/.bashrc
+ln -sf ~/dotfiles/linux/.bash_profile     ~/.bash_profile
+ln -sf ~/dotfiles/linux/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -sf ~/dotfiles/linux/.xmonad/xmobar.hs ~/.xmonad/xmobar.hs
+ln -sf ~/dotfiles/linux/.stalonetrayrc    ~/.stalonetrayrc
+ln -sf ~/dotfiles/linux/.xinitrc          ~/.xinitrc
+ln -sf ~/dotfiles/linux/.Xresources       ~/.Xresources
+ln -sf ~/dotfiles/linux/.vifm/vifmrc      ~/.vifm/vifmrc
+ln -sf ~/dotfiles/linux/.config.fish      ~/.config/fish/config.fish
+ln -sf ~/dotfiles/linux/.termite.config   ~/.config/termite/config
+ln -sf ~/dotfiles/linux/.skk-jisyo        ~/.skk-jisyo
+ln -sf ~/dotfiles/linux/.latexmkrc        ~/.latexmkrc
+
 ln -sf ~/dotfiles/sh/backlight/lighter.sh ~/lighter.sh
 ln -sf ~/dotfiles/sh/backlight/darker.sh  ~/darker.sh
 ln -sf ~/dotfiles/sh/git/push.sh          ~/push.sh
 ln -sf ~/dotfiles/sh/git/pull.sh          ~/pull.sh
-ln -sf ~/dotfiles/.xinitrc                ~/.xinitrc
-ln -sf ~/dotfiles/.Xresources             ~/.Xresources
-ln -sf ~/dotfiles/.vifm/vifmrc            ~/.vifm/vifmrc
-ln -sf ~/dotfiles/.config.fish            ~/.config/fish/config.fish
-ln -sf ~/dotfiles/.termite.config         ~/.config/termite/config
-ln -sf ~/dotfiles/.skk-jisyo              ~/.skk-jisyo
-ln -sf ~/dotfiles/.latexmkrc              ~/.latexmkrc
-ln -sf ~/dotfiles/.karabiner.json         ~/.config/karabiner/karabiner.json
+
+ln -sf ~/dotfiles/mac/.karabiner.json     ~/.config/karabiner/karabiner.json
