4e594b466c2c9b78419c86550072df43846c1aa2
x <x>
2020-11-12 16:01:37 +0900

    x

--- template/template.py
+++ template/template.py
@@ -1,9 +1,16 @@
 # filename.py
-# Last Change: 2020/01/07 (Tue) 22:29:19.
+# Last Change: 2020/11/12 (Thu) 15:56:07.
 
 import sys
 sys.dont_write_bytecode = True
 import math
+
+l_a=10
+l_b=2
+l_c=3
+l_1d=[0]*l_a
+l_2d=[[0]*l_b for i in range(l_a)]
+l_3d=[[[0]*l_c for i in range(l_b)] for j in range(l_a)]
 
 # while True:
 #     try:
@@ -11,4 +18,3 @@ import math
 #     except EOFError:
 #         break
 
-# lIst=[[0.0 for i in range(100)] for j in range(100)]
