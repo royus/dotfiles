18155b99abc59cd194fdc4f7cdd3e14c0606feb6
x <x>
2020-11-17 21:30:03 +0900

    x

--- template/template.py
+++ template/template.py
@@ -1,16 +1,83 @@
 # filename.py
-# Last Change: 2020/11/12 (Thu) 15:56:07.
+# Last Change: 2020/11/13 (Fri) 09:50:13.
 
 import sys
 sys.dont_write_bytecode = True
 import math
+import copy
 
-l_a=10
-l_b=2
-l_c=3
-l_1d=[0]*l_a
-l_2d=[[0]*l_b for i in range(l_a)]
-l_3d=[[[0]*l_c for i in range(l_b)] for j in range(l_a)]
+# l_a=1
+# l_b=2
+# l_c=3
+# l_1d=[0]*l_a
+# l_2d=[[0]*l_b for i in range(l_a)]
+# l_3d=[[[0]*l_c for i in range(l_b)] for j in range(l_a)]
+
+# c2i={
+#         'a':0 ,
+#         'b':1 ,
+#         'c':2 ,
+#         'd':3 ,
+#         'e':4 ,
+#         'f':5 ,
+#         'g':6 ,
+#         'h':7 ,
+#         'i':8 ,
+#         'j':9 ,
+#         'k':10,
+#         'l':11,
+#         'm':12,
+#         'n':13,
+#         'o':14,
+#         'p':15,
+#         'q':16,
+#         'r':17,
+#         's':18,
+#         't':19,
+#         'u':20,
+#         'v':21,
+#         'w':22,
+#         'x':23,
+#         'y':24,
+#         'z':25,
+#         }
+# i2c={
+#         0 :'a',
+#         1 :'b',
+#         2 :'c',
+#         3 :'d',
+#         4 :'e',
+#         5 :'f',
+#         6 :'g',
+#         7 :'h',
+#         8 :'i',
+#         9 :'j',
+#         10:'k',
+#         11:'l',
+#         12:'m',
+#         13:'n',
+#         14:'o',
+#         15:'p',
+#         16:'q',
+#         17:'r',
+#         18:'s',
+#         19:'t',
+#         20:'u',
+#         21:'v',
+#         22:'w',
+#         23:'x',
+#         24:'y',
+#         25:'z',
+#         }
+
+# M=10000
+# is_prime=[True]*(M+1)
+# is_prime[0]=False
+# is_prime[1]=False
+# for i in range(M+1):
+#     if is_prime[i]:
+#         for j in range(i*i,(M+1),i):
+#             is_prime[j]=False
 
 # while True:
 #     try:
